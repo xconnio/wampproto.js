ed85fa121f6ba4935e686bf623e99d0334248970
Mahad <mahad@xconn.io>
2024-06-15 17:33:50 +0500

    add unregistered message

---
+++ lib/messages/unregistered.ts
@@ -0,0 +1,49 @@
+import Message from "./message";
+import ValidationSpec from "./validation-spec";
+import {
+    validateMessage,
+    validateRequestID,
+} from "./util";
+
+interface IUnRegisteredFields {
+    readonly requestID: number;
+}
+
+class UnRegisteredFields implements IUnRegisteredFields {
+    constructor (private readonly _requestID: number) {}
+
+    get requestID(): number {
+        return this._requestID;
+    }
+}
+
+class UnRegistered implements Message {
+    static TYPE: number = 67;
+    static TEXT: string = "UNREGISTERED";
+
+    static VALIDATION_SPEC = new ValidationSpec(
+        2,
+        2,
+        UnRegistered.TEXT,
+        {1: validateRequestID},
+    )
+
+    constructor(private readonly _fields: IUnRegisteredFields) {}
+
+    get requestID(): number {
+        return this._fields.requestID;
+    }
+
+    static parse(msg: any[]): UnRegistered {
+        const f = validateMessage(msg, UnRegistered.TYPE, UnRegistered.TEXT, UnRegistered.VALIDATION_SPEC)
+        return new UnRegistered(new UnRegisteredFields(f.requestID));
+    }
+
+    marshal(): any[] {
+        return [UnRegistered.TYPE, this.requestID];
+    }
+
+    type(): number {
+        return UnRegistered.TYPE;
+    }
+}
