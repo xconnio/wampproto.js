44ac1652d399288fab70a3d9098f519c8ee007f2
Mahad <mahad@xconn.io>
2024-06-15 16:28:22 +0500

    add binary payload interface

--- lib/messages/message.ts
+++ lib/messages/message.ts
@@ -5,3 +5,9 @@ interface Message {
 }
 
 export default Message;
+
+export interface BinaryPayload {
+    payloadIsBinary(): boolean;
+    readonly payload: Uint8Array | null;
+    readonly payloadSerializer: number;
+}
